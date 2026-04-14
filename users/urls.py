95edd2be0a11b2aa6e3bda33829dee5ee87427ed
Benard-Ndiritu <ndiritubenard99@gmail.com>
2026-04-14 12:17:45 +0300

    Updated the reset password views

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from . import views
 from django.urls import path
-from .views import RegisterUser, LoginUser
+from .views import RegisterUser, LoginUser, RequestPasswordResetOTP, VerifyPasswordResetOTP, ResetPassword
+
 
 
 urlpatterns = [
     path("register/", RegisterUser.as_view(), name="register"),
     path("login/", LoginUser.as_view(), name="login"),
+    path("request-otp/", RequestPasswordResetOTP.as_view(), name="request-otp"),
+    path("verify-otp/", VerifyPasswordResetOTP.as_view(), name="verify-otp"),
+    path("reset-password/", ResetPassword.as_view(), name="reset-password"),
 ]
