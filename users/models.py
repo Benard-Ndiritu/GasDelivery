f580431afe0dfe4ae878a2e5e0b5fed525be906e
Benard-Ndiritu <ndiritubenard99@gmail.com>
2026-04-14 11:40:10 +0300

    Fix User model - add PermissionsMixin and admin methods

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+import random
+from django.utils import timezone
+from datetime import timedelta
+
 
 class UserManager(BaseUserManager):
     def create_user(self, phone_number, password=None, role='CUSTOMER'):
@@ -15,7 +19,8 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
-class User(AbstractBaseUser):
+
+class User(AbstractBaseUser, PermissionsMixin):  # Add PermissionsMixin
     ROLE_CHOICES = (
         ('CUSTOMER', 'Customer'),
         ('DEALER', 'Dealer'),
@@ -23,6 +28,7 @@ class User(AbstractBaseUser):
     )
 
     phone_number = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -33,3 +39,22 @@ class User(AbstractBaseUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+
+class PasswordResetOTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_used = models.BooleanField(default=False)
+
+    def is_valid(self):
+        return not self.is_used and timezone.now() < self.created_at + timedelta(minutes=10)
+
+    def __str__(self):
+        return f"OTP for {self.user.phone_number}"
