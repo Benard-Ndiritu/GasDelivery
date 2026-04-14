8b92945fb609d546063fd1b61faa80dd027522ea
Benard-Ndiritu <ndiritubenard99@gmail.com>
2026-04-14 10:58:28 +0300

    updated my email configuration

--- GasDelivery/settings.py
+++ GasDelivery/settings.py
@@ -172,10 +172,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email Configuration
+# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('ndiritubenard99@gmail.com')       # Your Gmail address
-EMAIL_HOST_PASSWORD = os.environ.get('qkxj rdbk rgky wauj')       # Gmail App Password
-DEFAULT_FROM_EMAIL = os.environ.get('ndiritubenard99@gmail.com')
+EMAIL_HOST_USER = 'ndiritubenard99@gmail.com'
+EMAIL_HOST_PASSWORD = 'qkxjrdbkrgkywauj'
+DEFAULT_FROM_EMAIL = 'ndiritubenard99@gmail.com'
