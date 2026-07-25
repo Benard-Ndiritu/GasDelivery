b3711941c0c8ce5094a9d41ca1745dcdada5fb5b
Benard-Ndiritu <ndiritubenard99@gmail.com>
2026-07-25 20:27:10 +0300

    Update dealer urls with application endpoints

--- dealers/urls.py
+++ dealers/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import CreateDealerProfile, ListDealers, DealerDetail, UpdateDealerProfile
+from .views import (
+    ApplyAsDealer,
+    ListDealerApplications,
+    ApproveDealerApplication,
+    DealerApplicationStatus,
+    ListDealers,
+    DealerDetail,
+    UpdateDealerProfile,
+)
 
 urlpatterns = [
     path('', ListDealers.as_view()),
-    path('create/', CreateDealerProfile.as_view()),
+    path('apply/', ApplyAsDealer.as_view()),
+    path('applications/', ListDealerApplications.as_view()),
+    path('applications/<int:pk>/action/', ApproveDealerApplication.as_view()),
+    path('application/status/', DealerApplicationStatus.as_view()),
     path('<int:pk>/', DealerDetail.as_view()),
     path('update/', UpdateDealerProfile.as_view()),
 ]
