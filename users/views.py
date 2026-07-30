9ab0ae2527d98f3634771e5a77ccb94104149c01
Benard-Ndiritu <ndiritubenard99@gmail.com>
2026-07-30 12:26:34 +0300

    Imported permissions

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User, PasswordResetOTP
 from .serializers import UserSerializer, LoginSerializer
